db9fce016675737ec91897a1d716eddcca71bd5e
Aryan7701 <aryanagrawal7701@gmail.com>
2023-04-13 00:47:43 +0530

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from"react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          {/* <Route exact path="/about" element={<About />} />
+          <Route exact path="/ContactUs" element={<ContactUs/>} /> */}
+        </Routes>
+      </div>
+    </Router>
+    </>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const NavBar=()=>{
+const Navbar= () =>{
     return(
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                <Link className="navbar-brand" to="#">Navbar</Link>
+                <Link className="navbar-brand" to="#">MyTravelExpi</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -34,3 +34,6 @@ const NavBar=()=>{
         </div>
     )
 }
+
+
+export default Navbar

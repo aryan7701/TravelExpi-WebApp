a6495157f954961a7306e3c53119e2216da5285b
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-01 01:27:32 +0530

    input some sample images

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,16 +3,20 @@ import PostItem from './PostItem';
 export class Post extends Component {
   render() {
     return (
-      <div>
+      // <div className='container my-4'>
         
       <div className="container my-4 mx-2">
         <h3>This is my Post section.</h3>
-      <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
-      <PostItem title="Krishna Temple" description="Near Uttar Pradesh, at the land of gods garden,there exists a krishna temple"/>
-      <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
-      <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
-      </div>
-      </div>
+            <div className="row">
+              <div className="col-md-6">
+              <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
+              <PostItem title="Krishna Temple" description="Near Uttar Pradesh, at the land of gods garden,there exists a krishna temple"/>
+              <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
+              <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
+              </div>
+            </div>
+        </div>
+      // </div>
     )
   }
 }
--- src/Components/PostItem.js
+++ src/Components/PostItem.js
@@ -4,8 +4,9 @@ export class PostItem extends Component {
   render() {
     let {title,description}=this.props;
     return (
-        <div className="card" style={{width:"18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card" style={{width:"20rem"}}>
+        {/* // <div className="card" style={{width:"24rem"?"18rem":"18rem"}}>if some error come in image response plz check also here. */}
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5Fc-T7lE2L-mBIw0oEEuH2R5uiOpv4aLF5w&usqp=CAU" className="card-img-top" alt="..."/>
         <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>

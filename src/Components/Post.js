a909ba1a34bc53b2ea54ead366d0f243d26dc334
Aryan7701 <aryanagrawal7701@gmail.com>
2023-05-01 00:30:08 +0530

    fixing the marging and padding as well as aligning different component

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -5,11 +5,13 @@ export class Post extends Component {
     return (
       <div>
         
-      <div className="ThisIsAPostComponent">This is a component</div>
+      <div className="container my-4 mx-2">
+        <h3>This is my Post section.</h3>
       <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
+      <PostItem title="Krishna Temple" description="Near Uttar Pradesh, at the land of gods garden,there exists a krishna temple"/>
+      <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
+      <PostItem title="Shiv Temple" description="Near Utrakhand, at the peak of gods valley,there exists a shiv temple"/>
+      </div>
       </div>
     )
   }
